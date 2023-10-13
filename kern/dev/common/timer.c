5b8eeb89da6214f2b9ce725c9ab9d2e8f7d962b2
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-10-12 23:31:32 -0700

    start on interrupt

--- kern/dev/common/timer.c
+++ kern/dev/common/timer.c
@@ -8,13 +8,17 @@ static char* const TIMER_BASE = (char*)(MMIO_BASE + 0x00003000);
 static const uint32_t TIMER_CS  =   0x00; // status
 static const uint32_t TIMER_CLO =   0x04; // counter low
 static const uint32_t TIMER_CHI =   0x08; // counter high
+static const uint32_t TIMER_C0  =   0x0c; // timer compare 0
+static const uint32_t TIMER_C1  =   0x10; // timer compare 1
+static const uint32_t TIMER_C2  =   0x14; // timer compare 2
+static const uint32_t TIMER_C3  =   0x18; // timer compare 3
 
 #define TIMER_REG(offset) (*(volatile uint32_t*)(TIMER_BASE + offset))
 
 void
 timer_init(void)
 {
-
+    TIMER_REG(TIMER_C1) = 10000; // 10ms
 }
 
 uint64_t timer_get(void) {
