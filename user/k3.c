ef9cd8f5fb814b0207c9e0450ba71d4958f71b9b
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-10-17 14:19:12 -0400

    small fixes

--- user/k3.c
+++ user/k3.c
@@ -18,15 +18,20 @@ typedef struct {
     uint32_t num_delays;
 } K3Resp;
 
+typedef struct {
+
+} K3Msg;
+
 void
 K3Client()
 {
     K3Resp resp_buf;
-    Send(MyParentTid(), "a", sizeof(char), (char*)&resp_buf, sizeof(K3Resp));
+    K3Msg dummy;
+    Send(MyParentTid(), (char*)&dummy, sizeof(K3Resp), (char*)&resp_buf, sizeof(K3Resp));
 
     Tid clock_server = WhoIs(CLOCK_ADDRESS);
     for (uint32_t i = 0; i < resp_buf.num_delays; ++i) {
-        Delay(clock_server, resp_buf.delay);
+        /* Delay(clock_server, resp_buf.delay); */
         println("Tid: %u, delay interval: %u, completed delays: %u", MyTid(), resp_buf.delay, i+1);
     }
 
@@ -42,17 +47,18 @@ K3()
     Create(6, &K3Client);
 
     int from_tid;
-    char* msg = alloc(sizeof(char));
+    K3Msg dummy;
 
     uint32_t delay[4] = {10, 23, 33, 71};
     uint32_t num_delays[4] = {20, 9, 6, 3};
 
     for (uint32_t i = 0; i < 4; i++) {
+        Receive(&from_tid, (char*)&dummy, sizeof(K3Msg));
+
         K3Resp resp = (K3Resp) {
             .delay = delay[i],
             .num_delays = num_delays[i]
         };
-        Receive(&from_tid, msg, sizeof(char));
         Reply(from_tid, (char*)&resp, sizeof(K3Resp));
     }
 
