5c84fcee33e4e9d55a914c9aa849ad7bbc170123
winterbeak <winterbeak@gmail.com>
2023-10-19 21:27:01 -0400

    Add IO primitive declarations

---
+++ user/io.c
@@ -0,0 +1,13 @@
+#include "io.h"
+
+int
+Getc(int tid, int channel)
+{
+    return 0;
+}
+
+int
+Putc(int tid, int channel, unsigned char ch)
+{
+    return 0;
+}
---
+++ user/io.h
@@ -0,0 +1,7 @@
+#ifndef __IO_H__
+#define __IO_H__
+
+int Getc(int tid, int channel);
+int Putc(int tid, int channel, unsigned char ch);
+
+#endif // __IO_H__
