a93c1c379ef5d519db61d8038b3acfdbc3a06bdf
winterbeak <winterbeak@gmail.com>
2023-10-19 22:12:12 -0400

    Implement Marklin IO server outline

--- user/io.c
+++ user/io.c
@@ -1,13 +1,110 @@
 #include "io.h"
+#include <trainsys.h>
+
+typedef enum {
+    IO_GETC = 1,
+    IO_PUTC,
+} IOMsgType;
+
+typedef struct {
+    IOMsgType type;
+    union {
+        struct {
+            int channel;
+        } getc;
+        struct {
+            int channel;
+            unsigned char ch;
+        } putc;
+    } data;
+} IOMsg;
+
+typedef struct {
+    IOMsgType type;
+
+    union {
+        struct {
+            unsigned char ch;
+        } getc;
+        struct { } putc;
+    } data;
+} IOResp;
 
 int
-Getc(int tid, int channel)
+Getc(Tid io_server, int channel)
 {
-    return 0;
+    IOResp resp_buf;
+    IOMsg send_buf = (IOMsg) {
+        .type = IO_GETC,
+        .data = {
+            .getc = {
+                .channel = channel
+            }
+        }
+    };
+
+    int ret = Send(io_server, (const char*)&send_buf, sizeof(IOMsg), (char*)&resp_buf, sizeof(IOResp));
+    if (ret < 0) {
+        println("[TID %d] WARNING, Getc()'s Send() call returned a negative value", MyTid());
+        return -1;
+    }
+    if (resp_buf.type != IO_GETC) {
+        println("[TID %d] WARNING, the reply to Getc()'s Send() call is not the right type", MyTid());
+        return -2;  // -2 is not in the kernel description for Getc()
+    }
+
+    return resp_buf.data.getc.ch;
 }
 
 int
-Putc(int tid, int channel, unsigned char ch)
+Putc(Tid io_server, int channel, unsigned char ch)
 {
+    IOResp resp_buf;
+    IOMsg send_buf = (IOMsg) {
+        .type = IO_PUTC,
+        .data = {
+            .putc = {
+                .channel = channel,
+                .ch = ch
+            }
+        }
+    };
+
+    int ret = Send(io_server, (const char*)&send_buf, sizeof(IOMsg), (char*)&resp_buf, sizeof(IOResp));
+    if (ret < 0) {
+        println("[TID %d] WARNING, Putc()'s Send() call returned a negative value", MyTid());
+        return -1;
+    }
+    if (resp_buf.type != IO_GETC) {
+        println("[TID %d] WARNING, the reply to Putc()'s Send() call is not the right type", MyTid());
+        return -2;  // -2 is not in the kernel description for Putc()
+    }
+
     return 0;
+}
+
+void
+marklinIO(void)
+{
+    IOMsg msg_buf;
+    IOResp reply_buf;
+    int from_tid;
+    for (;;) {
+        int msg_len = Receive(&from_tid, (char*)&msg_buf, sizeof(IOMsg));
+        if (msg_len < 0) {
+            println("[IO SERVER] Error when receiving");
+            continue;
+        }
+
+        if (msg_buf.type == IO_GETC) {
+            // Getc() implementation
+        }
+        else if (msg_buf.type == IO_PUTC) {
+            // Putc() implementation
+        }
+        else {
+            println("[IO SERVER] Invalid message type");
+            continue;
+        }
+    }
 }
--- user/io.h
+++ user/io.h
@@ -1,7 +1,9 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-int Getc(int tid, int channel);
-int Putc(int tid, int channel, unsigned char ch);
+int Getc(Tid tid, int channel);
+int Putc(Tid tid, int channel, unsigned char ch);
+
+void marklinIO(void);
 
 #endif // __IO_H__
