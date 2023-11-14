24a7c60de3190674107c7557dff4fbf0ff1d8924
winterbeak <winterbeak@gmail.com>
2023-11-13 23:16:28 -0500

    Add train 58 and 77 data

--- user/path/train_data.h
+++ user/path/train_data.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define TRAIN_DATA_TRAIN_COUNT 2
+#define TRAIN_DATA_TRAIN_COUNT 4
 #define TRAIN_DATA_SPEED_COUNT 4
 
 #define TRAIN_SPEED_SNAIL  5
@@ -11,15 +11,19 @@
 #define TRAIN_SPEED_MED    11
 #define TRAIN_SPEED_HIGH   14
 
-static const uint32_t TRAIN_DATA_TRAINS[TRAIN_DATA_TRAIN_COUNT] = {2, 47};
+static const uint32_t TRAIN_DATA_TRAINS[TRAIN_DATA_TRAIN_COUNT] = {2, 47, 58, 77};
 static const uint32_t TRAIN_DATA_SPEEDS[TRAIN_DATA_SPEED_COUNT] = {5, 8, 11, 14};
 static const uint32_t TRAIN_DATA_VEL[TRAIN_DATA_TRAIN_COUNT][TRAIN_DATA_SPEED_COUNT] = {
     {224, 387, 554, 650},  // 2
     {239, 390, 537, 578},  // 47
+    {66, 190, 371, 579},  // 58
+    {99, 232, 408, 612},  // 77
 };
 static const uint32_t TRAIN_DATA_STOP_DIST[TRAIN_DATA_TRAIN_COUNT][TRAIN_DATA_SPEED_COUNT] = {
     {275, 525, 770, 971},  // 2
     {284, 521, 709, 828},  // 47
+    {115, 348, 716, 1254},  // 58
+    {173, 447, 929, 1588},  // 77
 };
 
 uint32_t train_data_vel(uint32_t train, uint32_t speed);
