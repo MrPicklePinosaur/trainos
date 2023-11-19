dc8dad9f0e9ddb421d57a226ef9052c870735e68
winterbeak <winterbeak@gmail.com>
2023-11-18 20:47:26 -0500

    Implement short move interpolation function

--- user/path/train_data.c
+++ user/path/train_data.c
@@ -39,3 +39,30 @@ train_data_stop_dist(uint32_t train, uint32_t speed)
     uint32_t speed_index = get_speed_index(speed);
     return TRAIN_DATA_STOP_DIST[train_index][speed_index];
 }
+
+uint32_t
+train_data_short_move_time(uint32_t train, uint32_t dist)
+{
+    uint32_t train_index = get_train_index(train);
+    for (uint32_t i = 0; i < TRAIN_DATA_SHORT_MOVE_DIST_COUNT; i++) {
+        // For exact matches, return the exact time
+        if (TRAIN_DATA_SHORT_MOVE_DIST[train_index][i] == dist) {
+            return i*TRAIN_DATA_SHORT_MOVE_TIME_INCREMENT;
+        }
+        // For non matches, linearly interpolate the time from the two surrounding measurements
+        if (TRAIN_DATA_SHORT_MOVE_DIST[train_index][i] > dist) {
+            uint32_t prev_dist = TRAIN_DATA_SHORT_MOVE_DIST[train_index][i-1];
+            uint32_t next_dist = TRAIN_DATA_SHORT_MOVE_DIST[train_index][i];
+            uint32_t prev_time = (i-1)*TRAIN_DATA_SHORT_MOVE_TIME_INCREMENT;
+            uint32_t next_time = i*TRAIN_DATA_SHORT_MOVE_TIME_INCREMENT;
+            return prev_time + (next_time-prev_time)*(dist-prev_dist)/(next_dist-prev_dist);
+        }
+    }
+
+    // For distances above the highest measured distance, linearly extrapolate based on distance between last two recorded distances
+    uint32_t last_dist = TRAIN_DATA_SHORT_MOVE_DIST[train_index][TRAIN_DATA_SHORT_MOVE_DIST_COUNT-1];
+    uint32_t penultimate_dist = TRAIN_DATA_SHORT_MOVE_DIST[train_index][TRAIN_DATA_SHORT_MOVE_DIST_COUNT-2];
+    uint32_t last_time = TRAIN_DATA_SHORT_MOVE_TIME_INCREMENT*(TRAIN_DATA_SHORT_MOVE_DIST_COUNT-1);
+
+    return last_time + TRAIN_DATA_SHORT_MOVE_TIME_INCREMENT*(dist-last_dist)/(last_dist-penultimate_dist);
+}
--- user/path/train_data.h
+++ user/path/train_data.h
@@ -6,6 +6,9 @@
 #define TRAIN_DATA_TRAIN_COUNT 4
 #define TRAIN_DATA_SPEED_COUNT 4
 
+#define TRAIN_DATA_SHORT_MOVE_DIST_COUNT 17
+#define TRAIN_DATA_SHORT_MOVE_TIME_INCREMENT 250
+
 #define TRAIN_SPEED_SNAIL  5
 #define TRAIN_SPEED_LOW    8
 #define TRAIN_SPEED_MED    11
@@ -27,8 +30,15 @@ static const uint32_t TRAIN_DATA_STOP_DIST[TRAIN_DATA_TRAIN_COUNT][TRAIN_DATA_SP
     {115, 348, 716, 1254},  // 58
     {173, 447, 929, 1588},  // 77
 };
+static const uint32_t TRAIN_DATA_SHORT_MOVE_DIST[TRAIN_DATA_TRAIN_COUNT][TRAIN_DATA_SHORT_MOVE_DIST_COUNT] = {
+    {0, 10, 17, 33, 66, 97, 132, 186, 235, 305, 376, 475, 601, 725, 890, 992, 1118},  // 2
+    {0, 8, 24, 39, 73, 105, 152, 203, 269, 334, 435, 555, 730, 873, 1002, 1110, 1175},  // 47
+    {0, 10, 25, 72, 118, 192, 245, 290, 330, 384, 446, 477, 540, 575, 639, 672, 714},  // 58
+    {0, 17, 58, 97, 154, 209, 300, 351, 405, 458, 515, 570, 634, 690, 745, 805, 849},  // 77
+};
 
 uint32_t train_data_vel(uint32_t train, uint32_t speed);
 uint32_t train_data_stop_dist(uint32_t train, uint32_t speed);
+uint32_t train_data_short_move_time(uint32_t train, uint32_t dist);
 
 #endif // __TRAIN_DATA_H__
