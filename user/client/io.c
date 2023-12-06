68a039434d8d2f4e8cc1e2b32df499bf36fcf329
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-12-06 18:02:51 -0500

    add msg type

--- user/client/io.c
+++ user/client/io.c
@@ -4,28 +4,41 @@
 
 #include "kern/dev/uart.h"
 
+typedef enum {
+    CLIENT_MSG_SENSOR = 1,
+} ClientMsgType;
+
 typedef struct {
     usize train;
     usize speed;
-} IOMsg;
+} ClientMsg;
+
+void
+client_send_msg()
+{
+
+}
 
 void
 clientIoTask()
 {
     Tid clock_server = WhoIs(CLOCK_ADDRESS);
-    for (;;) {
+    usize i = 0;
+    for (;;++i) {
         Delay(clock_server, 100);
 
-        IOMsg msg = (IOMsg) {
+        ClientMsg msg = (ClientMsg) {
             .train = 2,
-            .speed = 10
+            .speed = i
         };
 
         const char MSG_START[2] = {0x69, 0x69};
-        const usize msg_len = sizeof(msg);
+        const u32 msg_len = sizeof(msg);
+        const u32 msg_type = CLIENT_MSG_SENSOR; // assume enum is u32
 
         uart_put_size(CONSOLE, (const char*)&MSG_START, sizeof(MSG_START));
         uart_put_size(CONSOLE, (const char*)&msg_len, sizeof(msg_len));
+        uart_put_size(CONSOLE, (const char*)&msg_type, sizeof(msg_type));
         uart_put_size(CONSOLE, (const char*)&msg, sizeof(msg));
 
     }
